993741cc19d1b201e903726435b0556f4882f125
Alexandria P <alxpettit@gmail.com>
2022-07-06 23:14:46 -0700

    red suggests applied

--- 01_exobody_ball_pit/Platform.gd
+++ 01_exobody_ball_pit/Platform.gd
@@ -1,4 +1,13 @@
 extends RigidBody2D
 
+onready var mouse_inside = false
+
 func _ready():
 	$CollisionPolygon2D.polygon = $Polygon2D.polygon
+
+
+func _on_Platform_mouse_entered():
+	mouse_inside = true
+
+func _on_Platform_mouse_exited():
+	mouse_inside = false
