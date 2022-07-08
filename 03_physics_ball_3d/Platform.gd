fbf25008c95f68b5c4da58a446f04c02106c433b
Alexandria P <alxpettit@gmail.com>
2022-07-08 14:39:03 -0700

    uwu

--- 03_physics_ball_3d/Platform.gd
+++ 03_physics_ball_3d/Platform.gd
@@ -4,7 +4,9 @@ extends RigidBody
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#$MeshInstance.shape()
+	var mesh: CubeMesh = $MeshInstance.mesh
+	var aabb: AABB = mesh.get_aabb()
+	print($CollisionObject.shape_owner_get_shape())
 	input_ray_pickable = true
 	# TODO: this convex collision doesn't seem to trigger mouse raytracing from camera
 	# - we know the spheres work, and they are using a more conventional collision shape object
@@ -12,5 +14,6 @@ func _ready():
 	# - figure out how to translate between a mesh shape and a boxagent
 	# ALSO CONSIDER:
 	# - rather than using this approach, we could totally create a FPS where you launch balls out of your face -- no raytracing required!
-	$MeshInstance.create_convex_collision()
+	# $MeshInstance.create_convex_collision()
 	#connect("input_event", self, "on_input_event")
+	
